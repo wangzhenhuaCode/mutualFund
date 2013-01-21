14c458f809c32e3cf096ee6014b3841232214dc1
yue ma <yuema@andrew.cmu.edu>
2013-01-21 17:07:31 -0500

    step1

--- mutualFund/src/Struts/Action/EmployeeAction.java
+++ mutualFund/src/Struts/Action/EmployeeAction.java
@@ -4,7 +4,31 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class EmployeeAction extends ActionSupport {
 //jkjk
-	public String customerLogin(){
-		return 
+	public String EmployeeLogin(){
+		return "employeeLoginSuccess";
+	}
+	public String EmployeeLogout(){
+		return "employeeLogoutSuccess";
+	}
+	public String EmployeeChangePassword(){
+		return "employeeLoginSuccess";
+	}
+	public String EmployeeAddNewFund(){
+		return "employeeLoginSuccess";
+	}
+	public String EmployeeAddNewCustomerAccount(){
+		return "employeeLoginSuccess";
+	}
+	public String EmployeeViewCustomerAccount(){
+		return "employeeLoginSuccess";
+	}
+	public String EmployeeResetCustomerPassword(){
+		return "employeeLoginSuccess";
+	}
+	public String EmployeeViewTransactionHistory(){
+		return "employeeLoginSuccess";
+	}
+	public String EmployeeCheckCustomerDeposit(){
+		return "employeeLoginSuccess";
 	}
 }
