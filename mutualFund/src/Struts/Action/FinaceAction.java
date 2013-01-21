48f3ba191ebe7eab4383bf2c37c223bd94cb5b08
wangzhenhuaCode <wangzhenhua.mail@gmail.com>
2013-01-21 16:43:18 -0500

    action

---
+++ mutualFund/src/Struts/Action/FinaceAction.java
@@ -0,0 +1,7 @@
+package Struts.Action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class FinaceAction extends ActionSupport {
+
+}
---
+++ mutualFund/src/Struts/Action/TransactionDayAction.java
@@ -0,0 +1,5 @@
+package Struts.Action;
+
+public class TransactionDayAction {
+
+}
