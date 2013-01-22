8088229bb4104ad4af38e25da3b3e43091cedc84
weihua cao <weihuac@andrew.cmu.edu>
2013-01-22 15:50:57 -0500

    finance

--- mutualFund/src/Struts/Action/FinanceAction.java
+++ mutualFund/src/Struts/Action/FinanceAction.java
@@ -1,11 +1,42 @@
 package Struts.Action;
 
+import java.util.Date;
+import java.util.Map;
+
+import Hibernate.DAO.ICustomerDAO;
+import Hibernate.DAO.IEmployeeDAO;
+import Hibernate.DAO.ITransactionDAO;
+import Hibernate.PO.Customer;
+import Hibernate.PO.Employee;
+import Hibernate.PO.Transaction;
+
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class FinanceAction extends ActionSupport {
+	private ICustomerDAO customerDAO;
+	private IEmployeeDAO employeeDAO;
+	private ITransactionDAO transactionDAO;
+	private String errorInfo;
+	private Customer customer;
+	private Employee employee;
+	private long amount;
 
 	public String requestCheck(){
-		return "Request Check succesfully";
+		ActionContext ctx=ActionContext.getContext();
+		Map<String,Object> session=ctx.getSession();
+		Customer customer=(Customer)session.get("customer");
+		if(amount<0) {
+			errorInfo="Amount cannot be negative!";
+			return "requestFailure";
+		}
+		Transaction transaction=new Transaction();
+		transaction.setCustomer(customer);
+		transaction.setTransactionType(Transaction.PENDING_DEPOSIT);
+		transaction.setAmount(amount);
+		transaction.setExecuteDate(new Date());
+		transactionDAO.save(transaction);
+		return "requestSuccess";
 	}
 	
 	public String financPage(){
@@ -17,10 +48,22 @@ public class FinanceAction extends ActionSupport {
 	}
 	
 	public String deposit(){
+		ActionContext ctx=ActionContext.getContext();
+		Map<String,Object> session=ctx.getSession();
+		Employee e=(Employee)session.get("employee");
 		return "Deposit";
 	}
 	
 	public String cancel(){
 		return "Cancel";
+	}
+	
+	
+	public void setAmount(long amount) {
+		this.amount = amount;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
 	}
 }
